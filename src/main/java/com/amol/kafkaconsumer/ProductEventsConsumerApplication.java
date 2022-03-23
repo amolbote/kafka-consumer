32a9547da631714bd6950d7c3fac5e4ff2670d90
amolbote <amol.a.bote@gmail.com>
2022-03-24 00:01:47 +0530

    Initial commit

---
+++ src/main/java/com/amol/kafkaconsumer/ProductEventsConsumerApplication.java
@@ -0,0 +1,13 @@
+package com.amol.kafkaconsumer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductEventsConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductEventsConsumerApplication.class, args);
+	}
+
+}
