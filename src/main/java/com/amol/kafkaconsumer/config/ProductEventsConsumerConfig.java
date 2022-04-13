3cf5cc63eda3f2fa4ee702470f96c43f00d05068
amolbote <amol.a.bote@gmail.com>
2022-04-14 00:58:29 +0530

    Run Zookeeper, 3 brokers from docker, producer, consumer and hit endpoint using postman (AddProductEvent)

---
+++ src/main/java/com/amol/kafkaconsumer/config/ProductEventsConsumerConfig.java
@@ -0,0 +1,12 @@
+package com.amol.kafkaconsumer.config;
+
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.kafka.annotation.EnableKafka;
+
+@Configuration
+@EnableKafka
+public class ProductEventsConsumerConfig {
+
+
+}
